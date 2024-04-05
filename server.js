596f7e3f7593466a2e979f7a9627ab421aab8988
Priest <Charleschukwuemeka47@gmail.com>
2024-04-05 16:36:48 +0100

    Fix: Made the installed HTMX to work

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //Import Other Needed Credentials
 import { PORT }  from "./config.js";
@@ -14,6 +19,9 @@ app.use(express.urlencoded({extended: true}));
 
 //Parse JSON bodies (as sent by API clients)
 app.use(express.json());
+
+// Serve static files from the node_modules directory
+app.use('/scripts', express.static(join(__dirname, 'node_modules')));
 
 
 //Start the server
