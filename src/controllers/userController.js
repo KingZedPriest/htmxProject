ba84dbad1105a97f0444050428a9cd4cd4224f0f
Priest <Charleschukwuemeka47@gmail.com>
2024-04-08 16:59:02 +0100

    Feat: Add image and a loader for the form when fetching

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,6 @@
-export const getUsers = (req, res) => {
-    const users = [
-        {id:1, name: "John Doe"},
-        {id:2, name: "Bob Williams"},
-        {id:3, name: "Hillary Clinton"},
-        {id:4, name: "Philip Stones"}
-    ];
+export const getUsers = async (req, res) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users = await response.json()
     res.send(`<h1 class="text-2xl font-bold my-4">Users</h1>
         <ul>
             ${users.map((user) => `<li>${user.name}</li>`).join("")}
